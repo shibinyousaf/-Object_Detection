8597512e8b878c910f2bc11c9be875e8b27b2814
shibinyousaf <shibin.yousafm@gmail.com>
2024-11-30 23:39:46 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # -Object_Detection
+
+## How to run
+
+```bash
+conda create -n signlang python=3.12.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'signLanguages',
+    version = '0.0.0',
+    author = 'Mohammed Shibin Yousaf',
+    author_email = 'shibin.yousafm@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
